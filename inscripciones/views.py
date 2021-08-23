b1b23adc542edd1ac78b045502509d2537af11ab
Faby <fabriciocabrera.56@gmailcom>
2021-08-23 18:46:10 -0500

    Incripciónes

--- inscripciones/views.py
+++ inscripciones/views.py
@@ -13,10 +13,12 @@ def inscripcionesform(request,pk):
     
     registro_form = RegistroForm(prefix='form_registro')
     inscripcion_form = InscripcionForm(prefix='form_inscripcion') 
+  
 
     contexto = {
         'registro_form':registro_form,
         'inscripcion_form':inscripcion_form
+       
     }
 
     if request.method == 'POST':
@@ -45,3 +47,44 @@ def inscripcionesform(request,pk):
 def lista_estudiantes(request):
 
     return render(request, "inscripciones/lista_inscritos.html", {"inscritos_curso":Inscripcion.objects.all})
+
+def eliminar_inscripcion(request,id):
+    incripcion = Inscripcion.objects.get(id = id)
+    incripcion.delete()
+    return redirect('inscripciones:lista_estudiantes')
+
+def editar_inscripcion(request, id):
+    inscripcion = Inscripcion.objects.get(id = id)
+    user = inscripcion.estudiante
+    if request.method == 'GET':
+        estudiante_form = EstudianteForm(instance=user)
+        inscripcion_form = InscripcionForm(instance=inscripcion) 
+        contexto = {
+        'estudiante_form':estudiante_form,
+        'inscripcion_form':inscripcion_form
+
+        }
+    else:
+        estudiante_form = EstudianteForm(request.POST, instance=user)
+        inscripcion_form = InscripcionForm(request.POST, instance=inscripcion) 
+        contexto = {
+        'estudiante_form':estudiante_form,
+        'inscripcion_form':inscripcion_form
+
+        }
+        if estudiante_form.is_valid() and inscripcion_form.is_valid():
+            estudiante = estudiante_form.save(commit=False)
+            estudiante.user = request.user
+            estudiante.save()
+            
+            inscripcion = inscripcion_form.save(commit=False)
+            inscripcion.estudiante = estudiante
+            inscripcion.save()
+            return redirect('inscripciones:lista_estudiantes')
+
+        else:
+            print('Error en los forms')
+            print(inscripcion_form.errors)
+            messages.error(request, estudiante_form.errors)
+            messages.error(request, inscripcion_form.errors)
+    return render(request,'inscripciones/editar_form.html', contexto)
